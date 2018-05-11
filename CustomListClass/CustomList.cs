2bf7dfeaec39c6781c74f0e501e795312ddf033e
ckloskey <you@example.com>
2018-05-11 07:49:06 -0500

    initial commit, code is very broken

---
+++ CustomListClass/CustomList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    public class CustomList<T> : Enumerable
+    {
+        //generic array
+        private T[] arr;
+        private T[] indexer = new T[100];
+
+        //capacity
+        private int capacity;
+        public CustomList()
+        {
+            this.capacity = 5;
+            arr = new T[capacity];
+        }
+
+        public T this[int capacity]
+        {
+            get => indexer[capacity];
+            set => indexer[capacity] = value;
+        }
+
+        public int Capacity { get; set; }
+        public T[] Arr { get => arr; set => arr = value; }
+
+        public void Add(T el)
+        {
+            int newCapacity = this.Capacity + 1;
+
+            for (int i = 0; i < this.Capacity; i++)
+            {
+                
+                arr[newCapacity] = el;
+
+            }
+
+        }
+    }
+}
---
+++ CustomListUnitTests/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClass;
+
+namespace CustomListUnitTests
+{
+    [TestClass]
+    public class CustomListTests
+    {
+        [TestMethod]
+        public void Add_AddItemToListCountIsOne_InputEqualsActual()
+        {   
+            //arrange
+            CustomList<int> c = new CustomList<int>();
+            int inputValue = 5;
+            //act
+            c.Add(inputValue);
+            int actualCount = c.Capacity;
+            //assert
+            Assert.AreEqual(inputValue, actualCount);
+        }
+
+        [TestMethod]
+        public void Add_AddItemsToArrayCheckSpecificIndex_()
+        {
+            //arrange
+            CustomList<int> c = new CustomList<int>();
+            int actualValue = 2;
+            //act
+            c.Add(1);
+            c.Add(2);
+            c.Add(3);
+            //assert
+            Assert.AreEqual(c.Arr[1], actualValue);
+        }
+
+        [TestMethod]
+        public void Add_CheckItemInList_()
+        {
+            //arrange
+            CustomList<int> c = new CustomList<int>();
+            int inputValue = 5;
+            //act
+            c.Add(inputValue);
+            //assert
+
+        }
+
+        [TestMethod]
+        public void Add_AddMultipleItemsAtOnce_()
+        {
+            //arrange
+            CustomList<int> c = new CustomList<int>();
+            int expectedCount = 6;
+            //act
+            c.Add(1);
+            c.Add(2);
+            c.Add(3);
+            c.Add(4);
+            c.Add(5);
+            c.Add(6);
+            int actualCount = c.Capacity;
+            //assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
